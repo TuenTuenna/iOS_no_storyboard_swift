94129f52e996b4cbb4d6c93d87ea84aba20e0ebe
Jeff, Jeong  정 의정 <35421421+TuenTuenna@users.noreply.github.com>
2020-06-01 01:45:42 +0900

    첫 커밋

---
+++ ProjectWithoutStoryboard_swift/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  ProjectWithoutStoryboard_swift
+//
+//  Created by Jeff Jeong on 2020/06/01.
+//  Copyright © 2020 Tuentuenna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        self.title = "뷰컨트롤러 No 스토리보드 - swift"
+        self.view.backgroundColor = .orange
+        
+    }
+
+
+}
+
